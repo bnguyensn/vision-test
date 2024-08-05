46dc154eb89fd54c8e8cd0f227a956101bbb2d0f
Binh Nguyen <b.nguyen.sn@gmail.com>
2024-08-05 23:38:59 +0100

    cloud fn code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import functions_framework
 from cloudevents.http.event import CloudEvent
 
+import vision_test
+
 
 @functions_framework.cloud_event
 def vision_trigger_from_storage(cloud_event: CloudEvent) -> None:
     print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
+    vision_test.get_movements()
